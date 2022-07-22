dda098ffb84c5b535028e7231eeac127f12880c4
Juan Carlos Hernandez <jchernandez9606@gmail.com>
2022-07-22 13:48:40 +0200

    routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SharedModule } from './shared/shared.module';
+import { AppRouterModule } from './app-router.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    AppRouterModule,
     BrowserModule,
-    SharedModule
+    SharedModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
